dfb01e5bb42ab1562c07c8d34cb5f2877da68490
lucas625 <lagc@cin.ufpe.br>
2021-02-14 16:36:34 -0300

    line intersecting triangle

--- ray-tracing/src/geometry/intersector/controller.go
+++ ray-tracing/src/geometry/intersector/controller.go
@@ -2,8 +2,10 @@ package intersector
 
 import (
 	"github.com/lucas625/Distributed-Ray-Tracing/ray-tracing/src/geometry/line"
+	"github.com/lucas625/Distributed-Ray-Tracing/ray-tracing/src/geometry/point"
 	"github.com/lucas625/Distributed-Ray-Tracing/ray-tracing/src/geometry/point_repository"
 	"github.com/lucas625/Distributed-Ray-Tracing/ray-tracing/src/geometry/triangle"
+	"github.com/lucas625/Distributed-Ray-Tracing/ray-tracing/src/geometry/vector"
 )
 
 // Controller is a class for controlling intersections.
@@ -14,59 +16,77 @@ import (
 type Controller struct {}
 
 // IntersectLineTriangle is a function to intersect triangles.
+// https://www.scratchapixel.com/lessons/3d-basic-rendering/ray-tracing-rendering-a-triangle/moller-trumbore-ray-triangle-intersection
 //
 // Parameters:
 // 	intersectingLine - The line.
 //  targetTriangle   - The target triangle.
+//  repository       - The point repository.
 //
 // Returns:
 //  The line t parameter (A + tV).
 //  The barycentric coordinates at that point.
 //  A flag checking if has intersection.
+//  An error.
 //
 func (*Controller) IntersectLineTriangle(intersectingLine *line.Line, targetTriangle *triangle.Triangle,
-	points *point_repository.PointRepository) (float64, []float64, bool) {
-	//rayOrigin := line.Start
-	//rayVector := line.Director
-	//
-	//EPSILON := 0.0000001;
-	//
-	//vertex0 := triang[0]
-	//vertex1 := triang[1]
-	//vertex2 := triang[2]
+	repository *point_repository.PointRepository) (float64, []float64, bool, error) {
+	rayOrigin := intersectingLine.GetStartingPoint()
+	rayVector := intersectingLine.GetVectorDirector()
 
+	EPSILON := 0.0000001
 
-	bCoord := make([]float64, 3)
-	//
-	//
-	//edge1 := ExtractVector(&vertex0, &vertex1)
-	//edge2 := ExtractVector(&vertex0, &vertex2)
-	//
-	//h := utils.VectorCrossProduct(&rayVector, &edge2)
-	//a := utils.DotProduct(&edge1, &h);
-	//
-	//if a > -EPSILON && a < EPSILON {
-	//	return 0,bCoord, false;    // This ray is parallel to this triangle.
-	//}
-	//f := 1.0/a
-	//
-	//s := ExtractVector(&vertex0,&rayOrigin)
-	//u := f * utils.DotProduct(&s, &h)
-	//if u < 0.0 || u > 1.0 {
-	//	return 0,bCoord,false
-	//}
-	//q := utils.VectorCrossProduct(&s,&edge1);
-	//v := f * utils.DotProduct(&rayVector,&q);
-	//if v < 0.0 || u + v > 1.0 {
-	//	return 0,bCoord,false
-	//}
-	//// At this stage we can compute t to find out where the intersection point is on the line.
-	//t := f * utils.DotProduct(&edge2, &q)
-	//if t > EPSILON && t < 1/EPSILON {
-	//	// outIntersectionPoint = rayOrigin + rayVector * t
-	//	bCoord = []float64{1-u-v, u, v}
-	//	return t,bCoord,true
-	//}
+	triangleController := triangle.Controller{}
 
-	return 0,bCoord,false
+	points, _ := triangleController.GetActualPoints(targetTriangle, repository)
+
+	pointController := point.Controller{}
+
+	firstEdge, _ := pointController.ExtractVector(points[0], points[1])
+	secondEdge, _ := pointController.ExtractVector(points[0], points[2])
+
+	vectorController := vector.Controller{}
+
+	h, err := vectorController.CrossProduct(rayVector, secondEdge)
+	if err != nil {
+		return 0, nil, false, err
+	}
+
+	a, _ := vectorController.DotProduct(firstEdge, h)
+
+	if a > -EPSILON && a < EPSILON {
+		return 0, nil, false, nil    // This ray is parallel to this triangle.
+	}
+	f := 1.0/a
+
+	s, _ := pointController.ExtractVector(points[0], rayOrigin)
+
+	dotProductSH, _ := vectorController.DotProduct(s, h)
+
+	u := f * dotProductSH
+	if u < 0.0 || u > 1.0 {
+		return 0, nil, false, nil
+	}
+
+	q, _ := vectorController.CrossProduct(s, firstEdge)
+
+	dotProductRayVectorQ, _ := vectorController.DotProduct(rayVector, q)
+
+	v := f * dotProductRayVectorQ
+
+	if v < 0.0 || u + v > 1.0 {
+		return 0, nil, false, nil
+	}
+
+	// At this stage we can compute t to find out where the intersection point is on the line.
+	dotProductSecondEdgeQ, _ := vectorController.DotProduct(secondEdge, q)
+
+	t := f * dotProductSecondEdgeQ
+	if t > EPSILON && t < 1/EPSILON {
+		// outIntersectionPoint = rayOrigin + rayVector * t
+		barycentricCoordinates := []float64{1-u-v, u, v}
+		return t, barycentricCoordinates, true, nil
+	}
+
+	return 0, nil, false, nil
 }
