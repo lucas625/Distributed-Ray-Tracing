d3a16c98392b56b5a719f5aa33a7340496e395e4
lucas625 <lagc@cin.ufpe.br>
2021-03-14 17:24:15 -0300

    path tracin run fixed

--- ray_tracing_controller/core/beans/__init__.py
+++
@@ -1,1 +0,0 @@
-from core.beans.color_matrix_bean import ColorMatrixBean
--- ray_tracing_controller/core/business/path_tracing_business.py
+++ ray_tracing_controller/core/business/path_tracing_business.py
@@ -1,12 +1,9 @@
 """Business object for running the path tracing."""
 
 import os
-import threading
 
 from django.conf import settings
 import requests
-
-from core.beans import ColorMatrixBean
 
 
 class PathTracingBusiness:
@@ -21,56 +18,15 @@ class PathTracingBusiness:
         """
         height = path_tracing_data.get('pixelScreen').get('height')
         width = path_tracing_data.get('pixelScreen').get('width')
-        color_matrix = ColorMatrixBean(height, width)
-        thread_count = 0
-        max_thread_count = 3
-        threads = []
-        window_size = 50
+        path_tracing_data['pathTracingParameters']['windowStartLine'] = 0
+        path_tracing_data['pathTracingParameters']['windowStartColumn'] = 0
+        path_tracing_data['pathTracingParameters']['windowEndLine'] = height
+        path_tracing_data['pathTracingParameters']['windowEndColumn'] = width
 
-        for line_index in range(height):
-            if thread_count < max_thread_count:
-                end_line = line_index+window_size if line_index + window_size <= height else height
-                arguments = (path_tracing_data, color_matrix, line_index, end_line, width)
-                line_index += window_size
-                thread = threading.Thread(target=PathTracingBusiness._perform_windowed_path_tracing, args=arguments)
-                threads.append(thread)
-                thread.start()
-            else:
-                for thread in threads:
-                    thread.join()
-                threads = []
-        for thread in threads:
-            thread.join()
-        image_response = requests.post(
-            os.path.join(settings.IMAGE_GENERATOR_ADDRESS, 'api', 'png'), json=color_matrix.to_dto())
-        return image_response.content
-
-    @staticmethod
-    def _perform_windowed_path_tracing(path_tracing_data, color_matrix, start_line, end_line, width):
-        """
-        :param dict path_tracing_data:
-        :param ColorMatrixBean color_matrix:
-        :param int start_line:
-        :param int end_line:
-        :param int width:
-        """
-        original_path_tracing_parameters = path_tracing_data.get('pathTracingParameters')
-        copied_data = dict(
-            pathTracingParameters=dict(
-                raysPerPixel=original_path_tracing_parameters.get('raysPerPixel'),
-                recursions=original_path_tracing_parameters.get('recursions'),
-                windowStartLine=start_line,
-                windowStartColumn=0,
-                windowEndLine=end_line,
-                windowEndColumn=width
-            ),
-            objects=path_tracing_data.get('objects'),
-            pixelScreen=path_tracing_data.get('pixelScreen'),
-            sceneCamera=path_tracing_data.get('sceneCamera'),
-            lights=path_tracing_data.get('lights')
-        )
         path_tracing_response = requests.post(
-            os.path.join(settings.RAY_TRACING_ADDRESS, 'path-tracing'), json=copied_data)
-        response_color_matrix = path_tracing_response.json().get('Colors')
-        color_matrix.set_color_matrix_by_window(response_color_matrix, start_line, 0, end_line, width)
+            os.path.join(settings.RAY_TRACING_ADDRESS, 'path-tracing'), json=path_tracing_data)
+        color_matrix = path_tracing_response.json()
 
+        image_response = requests.post(
+            os.path.join(settings.IMAGE_GENERATOR_ADDRESS, 'api', 'png'), json=color_matrix)
+        return image_response.content
