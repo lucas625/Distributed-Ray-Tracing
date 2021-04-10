54098a9a0e3c7b37fe243e0aa9084c671abd3267
lucas625 <lagc@cin.ufpe.br>
2021-04-10 13:24:06 -0300

    fixing number of same time requests

--- image_generator/entrypoint.sh
+++ image_generator/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python manage.py migrate
-gunicorn --bind 0.0.0.0:8082 --log-leve info --timeout 14400 --workers 4 image_generator.wsgi
+gunicorn --bind 0.0.0.0:8082 --log-leve info --timeout 14400 --workers 100 image_generator.wsgi
